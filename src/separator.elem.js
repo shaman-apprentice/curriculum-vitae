cae8a8b8847cc3416d357a9ace15757f26d51fd3
shaman-apprentice <Torsten.Knauf@gmx.de>
2019-10-20 23:11:12 +0200

    some quick adjustments

--- src/separator.elem.js
+++ src/separator.elem.js
@@ -1,6 +1,6 @@
 export class CurriculumVitaSeparator extends HTMLElement {
   connectedCallback() {
-    const hSize = this.getAttribute('h') || 3;
+    const hSize = this.getAttribute('h') || '3';
 
     this.innerHTML = `
       <h${hSize} class="h">${this.getAttribute('title')}</h${hSize}>
