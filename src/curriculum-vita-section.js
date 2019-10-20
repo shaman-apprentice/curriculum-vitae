653e42ffe838fe191cfb1c4020dbb3b2119ae17b
shaman-apprentice <Torsten.Knauf@gmx.de>
2019-10-20 12:31:19 +0200

    add skelton for sections

---
+++ src/curriculum-vita-section.js
@@ -0,0 +1,23 @@
+import { BaseComponent } from './BaseComponent.js';
+
+export class CurriculumVitaSection extends BaseComponent {
+  render(data) {
+    this.innerHTML = `
+      <h3>${data.title}</h3>
+      <hr />
+      <section name="content">
+        ${data.entries.map(renderSectionContent).join('')}
+      </section>
+    `;
+  }
+}
+
+function renderSectionContent(entry) {
+  return `
+    <section name="date">${entry.date}</section>
+    <section name="what">
+      <h4>${entry.title}</h4>
+      <p>${entry.description}</p>
+    </section> 
+  `;
+}
