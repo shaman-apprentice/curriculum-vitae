b871613962f68a2525fa76840f88c506f55cf328
shaman-apprentice <Torsten.Knauf@gmx.de>
2019-10-22 22:25:23 +0200

    make aboutMe more dynamic and align bdays
    at cost of soften separation of data and markup

--- src/about-me.elem.js
+++ src/about-me.elem.js
@@ -8,13 +8,10 @@ export class CurriculumVitaAboutMe extends BaseComponent {
       </section>
       <img name="QR-code-to-webpage" src="./assets/QR-code-to-webpage.svg"/>
       <section name="about-me">
-        ${data.reduce((acc, { name, lines }) => {
+        ${data.reduce((acc, { name, content }) => {
           acc += `
-            <p>${name}:</p>
-            <p>${lines.slice(1).reduce((acc, line) => {
-              acc += `<br/>${line}`;
-              return acc;
-            }, lines[0])}</p>
+            <span>${name}:</span>
+            ${content}
           `;
           return acc;
         }, '')}
